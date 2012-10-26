f01072e6e6f27ccc19d5fa776bfde18a19cd087d
Andrew Downing <ad510@users.sourceforge.net>
2012-10-25 23:59:32 -0700

    add rotation vars to movement data structure

--- move_test/Movement.java
+++ move_test/Movement.java
@@ -3,14 +3,18 @@ import java.io.*;
 
 public class Movement implements Serializable {
   private Point2D.Double startPos;
+  private double startRot;
   private int startTime;
   private Point2D.Double endPos;
+  private double endRot;
   private int endTime;
 
-  public Movement(Point2D.Double newStartPos, int newStartTime, Point2D.Double newEndPos, int newEndTime) {
+  public Movement(Point2D.Double newStartPos, double newStartRot, int newStartTime, Point2D.Double newEndPos, double newEndRot, int newEndTime) {
     startPos = newStartPos;
+    startRot = newStartRot;
     startTime = newStartTime;
     endPos = newEndPos;
+    endRot = newEndRot;
     endTime = newEndTime;
   }
 
