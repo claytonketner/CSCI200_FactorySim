5491e2a0380569a36669343e4e49dbaa770b0c5d
Andrew Downing <ad510@users.sourceforge.net>
2012-10-27 16:06:40 -0700

    add comments to Movement class

--- andrew_helper_classes/Movement.java
+++ andrew_helper_classes/Movement.java
@@ -1,15 +1,25 @@
 import java.awt.geom.*;
 import java.io.*;
 
+/** represents the movement of an object that starts at a specified position and rotation,
+    moves at constant velocity to a specified end position and rotation, then stops */
 public class Movement implements Serializable {
+  /** position at beginning of this move */
   private Point2D.Double startPos;
+  /** counterclockwise rotation in radians at beginning of this move */
   private double startRot;
+  /** time that this move starts, in milliseconds after the simulation started */
   private int startTime;
+  /** position at end of this move */
   private Point2D.Double endPos;
+  /** counterclockwise rotation in radians at end of this move */
   private double endRot;
+  /** time that this move ends, in milliseconds after the simulation started */
   private int endTime;
 
-  public Movement(Point2D.Double newStartPos, double newStartRot, int newStartTime, Point2D.Double newEndPos, double newEndRot, int newEndTime) {
+  /** constructor that sets all instance variables to specified values */
+  public Movement(Point2D.Double newStartPos, double newStartRot, int newStartTime,
+                  Point2D.Double newEndPos, double newEndRot, int newEndTime) {
     startPos = newStartPos;
     startRot = newStartRot;
     startTime = newStartTime;
@@ -18,6 +28,7 @@ public class Movement implements Serializable {
     endTime = newEndTime;
   }
 
+  /** returns position at specified time */
   public Point2D.Double getPosWhen(int time) {
     if (time <= startTime) {
       return startPos;
@@ -29,6 +40,7 @@ public class Movement implements Serializable {
                               startPos.getY() + (endPos.getY() - startPos.getY()) / (endTime - startTime) * (time - startTime));
   }
 
+  /** returns rotation at specified time */
   public double getRotWhen(int time) {
     if (time <= startTime) {
       return startRot;
