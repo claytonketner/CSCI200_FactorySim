78e5dcc1c2c01b5b9f219562571b0265d1a9560b
Andrew Downing <ad510@users.sourceforge.net>
2012-10-27 20:59:50 -0700

    add ConnectPanel.java and modify TestClient.java to use it

--- TestClient.java
+++ TestClient.java
@@ -1,4 +1,5 @@
 // client modified from Andrew's lab 6 made with the sole purpose of testing Server.java
+// search "ALL CLIENTS" for things that all clients need to do to communicate with server
 // delete this after we start the actual factory project code
 
 import java.io.*;
@@ -8,31 +9,35 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class TestClient extends JFrame implements ActionListener, Networked {
-  private NetComm netComm;
+  private NetComm netComm; // ALL CLIENTS must have a NetComm instance
 
+  private CardLayout layout;
+  private ConnectPanel panelConnect; // ALL CLIENTS must have a connect to server panel
+  private JPanel panelChat;
   private JTextField text;
   private JButton button;
   private JLabel label;
 
   public TestClient() {
-    // set up networking (assume server is on localhost, but final project cannot make this assumption)
-    try {
-      netComm = new NetComm(new Socket("localhost", Server.Port), this);
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-      System.exit(1);
-    }
-    // set up GUI
-    setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+    // set up chat GUI
+    // (normally the chat panel should have its own class, but this is a test program so I'm being lazy)
+    panelChat = new JPanel();
+    panelChat.setLayout(new BoxLayout(panelChat, BoxLayout.Y_AXIS));
     text = new JTextField();
     button = new JButton("Submit");
     label = new JLabel(" ");
-    add(text);
-    add(button);
-    add(label);
+    panelChat.add(text);
+    panelChat.add(button);
+    panelChat.add(label);
     button.addActionListener(this);
-    setSize(400, 200);
+    // set up window
+    layout = new CardLayout();
+    setLayout(layout);
+    // ALL CLIENTS must instantiate their connect to server panel and add it to the JFrame
+    panelConnect = new ConnectPanel(this);
+    add(panelConnect, "connect");
+    add(panelChat, "chat");
+    setSize(600, 400);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
   }
@@ -41,12 +46,16 @@ public class TestClient extends JFrame implements ActionListener, Networked {
     TestClient client = new TestClient();
   }
 
+  // ALL CLIENTS must implement Networked and have this method (though what the method does may be different)
   public void msgReceived(Object msgObj, NetComm sender) {
-    if (msgObj instanceof CloseConnectionMsg) {
-      System.out.println("Unexpectedly disconnected from server");
-      System.exit(1);
+    if (msgObj instanceof CloseConnectionMsg) { // ALL CLIENTS must handle receiving CloseConnectionMsg
+      netComm.close();
+      netComm = null;
+      panelConnect.reset();
+      panelConnect.setActionMsg("Unexpectedly disconnected from server");
+      layout.show(this.getContentPane(), "connect");
     }
-    else if (msgObj instanceof String) {
+    else if (msgObj instanceof String) { // for the actual factory project we will never send strings, and instead have dedicated Msg objects like CloseConnectionMsg
       label.setText((String)msgObj);
     }
     else {
@@ -55,7 +64,18 @@ public class TestClient extends JFrame implements ActionListener, Networked {
   }
 
   public void actionPerformed(ActionEvent ae) {
-    if (ae.getSource() == button) {
+    // ALL CLIENTS must implement ActionListener and instantiate their NetComm when connect panel says user pressed "Connect to Server" button
+    if (ae.getSource() == panelConnect) {
+      try {
+        netComm = new NetComm(new Socket(panelConnect.getServerAddress(), Server.Port), this);
+        layout.show(this.getContentPane(), "chat");
+      }
+      catch (Exception ex) {
+        netComm = null;
+        panelConnect.setActionError("Could not connect to server; check that it was entered correctly");
+      }
+    }
+    else if (ae.getSource() == button) {
       netComm.write(text.getText());
       text.setText("");
     }
