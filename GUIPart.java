025b8b1ba483986738865f298b08233cf4306153
Clayton <cketner@usc.edu>
2012-10-31 22:53:54 -0700

    Removed bulky draw code, replaced with Painter static draw method

--- GUIPart.java
+++ GUIPart.java
@@ -1,6 +1,5 @@
-import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
 import java.util.TreeMap;
 
 import javax.swing.ImageIcon;
@@ -20,9 +19,9 @@ public class GUIPart
 		image = allPartImages.get(part.getName());
 		movement = new Movement(new Point2D.Double(x,y), 0);
 	}
-	
-	public void draw(Graphics g, long currentTime)
+
+	public void draw(Graphics2D g, long currentTime)
 	{
-		g.drawImage(image.getImage(), (int)(movement.calcPos(currentTime).x), (int)(movement.calcPos(currentTime).y), null);
+		Painter.draw(g, image, currentTime, movement);
 	}
 }
