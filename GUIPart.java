f0e0995314a9dcbd6e1c0bf5954237fdd2998e3a
Clayton <cketner@usc.edu>
2012-11-01 00:54:31 -0700

    Changed to arraylist of images, added code for loading the images if they arent already

--- GUIPart.java
+++ GUIPart.java
@@ -1,6 +1,6 @@
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
-import java.util.TreeMap;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 
@@ -10,18 +10,36 @@ public class GUIPart
 	public Part part;
 	public Movement movement;
 	
-	private TreeMap<String, ImageIcon> allPartImages; // Not sure where this should be, but I guess it can stay here for V0
+	// Image constants
+	public static final int RAISIN = 0;
+	
+	private static ArrayList<ImageIcon> allPartImages; // Not sure where this should be, but I guess it can stay here for V0
 	private ImageIcon image;
 	
-	public GUIPart(Part part, double x, double y)
+	
+	public GUIPart(Part part, int imageID, double x, double y)
 	{
 		this.part = part;
-		image = allPartImages.get(part.getName());
 		movement = new Movement(new Point2D.Double(x,y), 0);
+		
+		if (allPartImages == null)
+		{
+			// Images need to be loaded
+			allPartImages = new ArrayList<ImageIcon>();
+			allPartImages.add(RAISIN, new ImageIcon("images/parts/raisin.png"));
+		}
+		
+		try {
+			image = allPartImages.get(imageID);
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("Part image doesn't exist!");
+			e.printStackTrace();
+		}
 	}
 
+	
 	public void draw(Graphics2D g, long currentTime)
 	{
-		Painter.draw(g, image, currentTime, movement);
+		Painter.draw(g, image, -1, 25, currentTime, movement);
 	}
 }
