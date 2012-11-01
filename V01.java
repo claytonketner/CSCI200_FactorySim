cb04fd107c0779d58a7870c423280cb1980162db
Clayton <cketner@usc.edu>
2012-11-01 15:58:46 -0700

    Slight update to test

--- V01.java
+++ V01.java
@@ -1,5 +1,7 @@
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
 
 import javax.swing.JPanel;
 
@@ -10,8 +12,10 @@ public class V01 extends JPanel
 	
 	public V01()
 	{
+		this.setPreferredSize(new Dimension(400,600));
 		Part p = new Part("p1", "a random part", 5);
 		gp = new GUIPart(p, GUIPart.RAISIN, 100, 100);
+		gp.movement = new Movement(new Point2D.Double(100,100), 0, System.currentTimeMillis(), new Point2D.Double(200,200), 10, System.currentTimeMillis()+5000);
 	}
 	
 	public void paint(Graphics g)
