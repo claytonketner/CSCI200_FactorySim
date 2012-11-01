0072fed3818a061ccee38622b471619cd5430e65
Clayton <cketner@usc.edu>
2012-11-01 00:55:43 -0700

    Added static modifier to kit status ints

--- Kit.java
+++ Kit.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 public class Kit 
 {
 	// Kit statuses
-	public final int INCOMPLETE = 0;
-	public final int COMPLETE = 1;
-	public final int INQUEUE = 2;
-	public final int PARTMISSING = 3;
+	public static final int INCOMPLETE = 0;
+	public static final int COMPLETE = 1;
+	public static final int INQUEUE = 2;
+	public static final int PARTMISSING = 3;
 	
 	private int number;
 	private String name, description;
