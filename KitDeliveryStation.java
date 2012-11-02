ff40487ff2530f78e4016dc0518aafb36c592f43
Clayton <cketner@usc.edu>
2012-11-02 15:03:28 -0700

    Removed GUIKitDeliveryStation object

--- KitDeliveryStation.java
+++ KitDeliveryStation.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class KitDeliveryStation implements Serializable {
-	GUIKitDeliveryStation guiKitDeliveryStation;
 	ArrayList<Pallet> pallets;
 	
 	public KitDeliveryStation() {
-		guiKitDeliveryStation = new GUIKitDeliveryStation();
 		pallets = new ArrayList<Pallet>();
 	}
 }
