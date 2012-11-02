f2f850fbae61a5ff9c5ad3310841fade7b273ba1
Justin Sanders <justinsa@usc.edu>
2012-11-01 23:06:18 -0700

    added GUIPallet instantiation

--- Pallet.java
+++ Pallet.java
@@ -3,10 +3,12 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Pallet implements Serializable {
+	GUIPallet guiPallet;
 	Kit kit;
 	boolean hasKit;
 	
 	public Pallet() {
+		guiPallet = new GUIPallet();
 		kit = new Kit();
 		hasKit = false;
 	}
