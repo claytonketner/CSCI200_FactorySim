01cb342d0f3bcf644c41972556ab1cc0338ef261
Clayton <cketner@usc.edu>
2012-11-02 14:56:20 -0700

    Removed GUIPartRobot object

--- PartRobot.java
+++ PartRobot.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class PartRobot implements Serializable {
-	GUIPartRobot guiPartRobot;
 	ArrayList<Part> partsInGripper;
 	Point2D.Double position;
 	Movement movement;
 	
 	public PartRobot() {
-		guiPartRobot = new GUIPartRobot();
 		partsInGripper = new ArrayList<Part>();
 		position = new Point2D.Double( 300, 300 ); //these values are random
 		movement = new Movement( position, 0 );
