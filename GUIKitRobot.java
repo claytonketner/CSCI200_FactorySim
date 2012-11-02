d960db6b155751f5cfe0c6c557463f586bdfbfba
Clayton <cketner@usc.edu>
2012-11-02 15:02:30 -0700

    Added public modifier to KitRobot and Movement objects

--- GUIKitRobot.java
+++ GUIKitRobot.java
@@ -6,8 +6,8 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class GUIKitRobot implements Serializable
 {
-	KitRobot kitRobot;
-	Movement movement; // The kit robot doesn't use this for movement - only to access its goal and desired end time because of the complex calculations required
+	public KitRobot kitRobot;
+	public Movement movement; // The kit robot doesn't use this for movement - only to access its goal and desired end time because of the complex calculations required
 	
 	
 	public GUIKitRobot(KitRobot kitRobot, double x, double y)
