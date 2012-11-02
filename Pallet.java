85407aca23b3e2b8322d2816b6c50f8d161dcb7c
Clayton <cketner@usc.edu>
2012-11-02 14:56:00 -0700

    Removed GUIPallet object

--- Pallet.java
+++ Pallet.java
@@ -3,12 +3,10 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Pallet implements Serializable {
-	GUIPallet guiPallet;
 	Kit kit;
 	boolean hasKit;
 	
 	public Pallet() {
-		guiPallet = new GUIPallet();
 		kit = new Kit();
 		hasKit = false;
 	}
