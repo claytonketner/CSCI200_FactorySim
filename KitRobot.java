5c70308e9eb9615c6bf222b407f149972ef8ae48
Clayton <cketner@usc.edu>
2012-11-02 14:54:45 -0700

    Removed GUIKitRobot object

--- KitRobot.java
+++ KitRobot.java
@@ -4,13 +4,11 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class KitRobot implements Serializable {
-	GUIKitRobot guiKitRobot;
 	Kit kit;
 	Point2D.Double position;
 	Movement movement;
 	
 	public KitRobot() {
-		guiKitRobot = new GUIKitRobot();
 		kit = new Kit();
 		position = new Point2D.Double( 300, 300 ); //these values are random
 		movement = new Movement( position, 0 );
