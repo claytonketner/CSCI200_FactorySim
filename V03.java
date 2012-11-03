3dc686d5301b3d4e054aa75e4f9be50036e7dc61
antq888 <antq888@yahoo.com>
2012-11-03 13:53:04 -0700

    moving parts now react to diverter

--- V03.java
+++ V03.java
@@ -31,36 +31,62 @@ public class V03 extends JPanel
 		gl = new GUILane( lane1.getComboLane(), 5, 190,10 );
 		
 		p = new Part("p", "a random part", 5);
-		//gp1 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 10);
-		//gp1.movement = new Movement(gp1.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,10), 10, System.currentTimeMillis()+5000);
 	}
 	
 	public void paint(Graphics gfx)
 	{
 		Graphics2D g = (Graphics2D)gfx;
 		
-		//top lane moving parts
-		if (paintCount % 100 == 0) {
-			gp1 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 20);
-			gp1.movement = new Movement(gp1.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,20), 10, System.currentTimeMillis()+5000);
+		
+		if( lane1.getLane() == 1 ){ //if top lane
+			//top lane moving parts
+			if (paintCount % 100 == 0) {
+				gp1 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 20);
+				gp1.movement = new Movement(gp1.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,20), 10, System.currentTimeMillis()+5000);
+			}
+			
+			if (paintCount % 100 == 25) {
+				gp2 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 20);
+				gp2.movement = new Movement(gp2.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,20), 10, System.currentTimeMillis()+5000);
+			}
+			
+			if (paintCount % 100 == 50) {
+				gp3 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 20);
+				gp3.movement = new Movement(gp3.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,20), 10, System.currentTimeMillis()+5000);
+			}
+			
+			if (paintCount % 100 == 75) {
+				gp4 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 20);
+				gp4.movement = new Movement(gp4.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,20), 10, System.currentTimeMillis()+5000);
+			}
+		} else { //bottom lane
+			//bot lane moving parts
+			if (paintCount % 100 == 0) {
+				gp1 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 70);
+				gp1.movement = new Movement(gp1.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,70), 10, System.currentTimeMillis()+5000);
+			}
+			
+			if (paintCount % 100 == 25) {
+				gp2 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 70);
+				gp2.movement = new Movement(gp2.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,70), 10, System.currentTimeMillis()+5000);
+			}
+			
+			if (paintCount % 100 == 50) {
+				gp3 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 70);
+				gp3.movement = new Movement(gp3.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,70), 10, System.currentTimeMillis()+5000);
+			}
+			
+			if (paintCount % 100 == 75) {
+				gp4 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 70);
+				gp4.movement = new Movement(gp4.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,70), 10, System.currentTimeMillis()+5000);
+			}
 		}
 		
-		if (paintCount % 100 == 25) {
-			gp2 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 20);
-			gp2.movement = new Movement(gp2.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,20), 10, System.currentTimeMillis()+5000);
+		//change lane every 40 and 90 (time for diverter to move later?)
+		if( paintCount % 100 == 40 || paintCount % 100 == 90){
+			lane1.divert();
 		}
 		
-		
-		//bot lane moving parts
-		if (paintCount % 100 == 50) {
-			gp3 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 70);
-			gp3.movement = new Movement(gp3.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,70), 10, System.currentTimeMillis()+5000);
-		}
-		
-		if (paintCount % 100 == 75) {
-			gp4 = new GUIPart(p, Painter.ImageEnum.CORNFLAKE, 490, 70);
-			gp4.movement = new Movement(gp4.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(190,70), 10, System.currentTimeMillis()+5000);
-		}
 		gl.draw(g, System.currentTimeMillis());
 		
 		if( gp1 != null )
