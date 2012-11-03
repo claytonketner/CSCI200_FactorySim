181156c77317bbdd3d37308bd4f0738a6f2f4695
antq888 <antq888@yahoo.com>
2012-11-03 13:06:13 -0700

    lane is drawn

--- V03.java
+++ V03.java
@@ -1,10 +1,28 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 
 public class V03 extends JPanel
 {
+	WholeLane lane1;
+	GUILane test;
+	
 	public V03()
 	{
+		this.setPreferredSize(new Dimension(800,400));	
+		Painter.loadImages();
 		
+		lane1 = new WholeLane();
+		test = new GUILane( lane1.getComboLane(), 5, 190,10 );
 	}
+	
+	public void paint(Graphics gfx)
+	{
+		Graphics2D g = (Graphics2D)gfx;
+		
+		test.draw(g, System.currentTimeMillis());
+	}
+	
 }
