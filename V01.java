9a8ad3db771658ea8acaf86294891dcb11f789f0
Clayton <cketner@usc.edu>
2012-11-02 18:09:38 -0700

    Now tests the KitDeliverySystem GUI - check it out!

--- V01.java
+++ V01.java
@@ -10,6 +10,9 @@ import javax.swing.JPanel;
 public class V01 extends JPanel
 {
 	GUIPart gp;
+	GUIKitDeliveryStation guiKitDeliveryStation;
+	
+	int paintCount = 0;
 	
 	public V01()
 	{
@@ -20,13 +23,24 @@ public class V01 extends JPanel
 		Part p = new Part("p1", "a random part", 5);
 		gp = new GUIPart(p, Painter.ImageEnum.RAISIN, 100, 100);
 		gp.movement = new Movement(gp.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(200,200), 10, System.currentTimeMillis()+5000);
-	}
-	
-	public void paint(Graphics g)
-	{
 		
-		gp.draw((Graphics2D)g, System.currentTimeMillis());
+		guiKitDeliveryStation = new GUIKitDeliveryStation(new KitDeliveryStation(), new GUILane(new Lane(), 3, 190,10), new GUILane(new Lane(), 3, 10, 10), 10, 10);
 	}
 	
+	public void paint(Graphics gfx)
+	{
+		Graphics2D g = (Graphics2D)gfx;
+		
+		guiKitDeliveryStation.draw(g, System.currentTimeMillis());
+		gp.draw(g, System.currentTimeMillis());
+		
+		if (paintCount % 100 == 0)
+			if (guiKitDeliveryStation.inConveyor.lane.isLaneOn())
+				guiKitDeliveryStation.inConveyor.lane.turnOff();
+			else
+				guiKitDeliveryStation.inConveyor.lane.turnOn();
+		
+		paintCount++;
+	}
 	
 }
