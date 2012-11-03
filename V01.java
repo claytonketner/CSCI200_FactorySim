5110caa73624460cbc1dc6c46cde111ca749f408
Clayton <cketner@usc.edu>
2012-11-03 13:23:17 -0700

    Added Kit robot and kit stand

--- V01.java
+++ V01.java
@@ -11,28 +11,36 @@ public class V01 extends JPanel
 {
 	GUIPart gp;
 	GUIKitDeliveryStation guiKitDeliveryStation;
+	GUIKitRobot guiKitRobot;
+	GUIKitStand guiKitStand;
 	
 	int paintCount = 0;
 	
 	public V01()
 	{
-		this.setPreferredSize(new Dimension(400,600));
+		this.setPreferredSize(new Dimension(500,600));
 		
 		Painter.loadImages();
 		
 		Part p = new Part("p1", "a random part", 5);
-		gp = new GUIPart(p, Painter.ImageEnum.RAISIN, 100, 100);
+		gp = new GUIPart(p, Painter.ImageEnum.RAISIN, 0, 0);
 		gp.movement = new Movement(gp.movement.calcPos(System.currentTimeMillis()), 0, System.currentTimeMillis(), new Point2D.Double(200,200), 10, System.currentTimeMillis()+5000);
 		
 		guiKitDeliveryStation = new GUIKitDeliveryStation(new KitDeliveryStation(), new GUILane(new ComboLane(), 3, 190,10), new GUILane(new ComboLane(), 3, 10, 10), 10, 10);
+		
+		guiKitRobot = new GUIKitRobot(new KitRobot());
+		guiKitStand = new GUIKitStand(new KitStand());
 	}
 	
 	public void paint(Graphics gfx)
 	{
 		Graphics2D g = (Graphics2D)gfx;
+		long currentTime = System.currentTimeMillis();
 		
-		guiKitDeliveryStation.draw(g, System.currentTimeMillis());
-		gp.draw(g, System.currentTimeMillis());
+		guiKitDeliveryStation.draw(g, currentTime);
+		gp.draw(g, currentTime);
+		guiKitRobot.draw(g, currentTime);
+		guiKitStand.draw(g, currentTime);
 		
 		if (paintCount % 100 == 0)
 			if (guiKitDeliveryStation.inConveyor.lane.isLaneOn())
