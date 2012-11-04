71baccaefb7018665e07459af703daddf84d18d9
Clayton <cketner@usc.edu>
2012-11-04 13:15:07 -0800

    Added calculations to point at target location;

--- GUIKitRobot.java
+++ GUIKitRobot.java
@@ -11,18 +11,20 @@ public class GUIKitRobot implements Serializable
 	/** Set the startPos of this Movement to the robot's end point destination */
 	public Movement movement; // The kit robot doesn't use this for movement - only to access its goal and desired end time because of the complex calculations required
 	
+	private GUIKit kit;
 	private Movement baseMove, armMove, handMove;
+	private final int baseStartX = 300;
+	private final int baseStartY = 270;
 	
 	
 	public GUIKitRobot(KitRobot kitRobot)
 	{
 		this.kitRobot = kitRobot;
-		movement = new Movement(new Point2D.Double(300,0), 0);
-		baseMove = new Movement(new Point2D.Double(300,270), 0);
+		kit = null;
+		movement = new Movement(new Point2D.Double(baseStartX, baseStartY), 0);
+		baseMove = new Movement(new Point2D.Double(baseStartX, baseStartY), 0);
 		armMove = new Movement(new Point2D.Double(0,0), 0);
 		handMove = new Movement(new Point2D.Double(0,0), 0);
-		
-		armMove = new Movement(movement.getStartPos(), 0, System.currentTimeMillis(), movement.getStartPos(), 100, System.currentTimeMillis()+100000);
 	}
 
 	public void draw(Graphics2D g, long currentTime)
@@ -32,14 +34,89 @@ public class GUIKitRobot implements Serializable
 		Painter.draw(g, Painter.ImageEnum.ROBOT_BASE, 75, -1, currentTime, baseMove, true);
 		Painter.draw(g, Painter.ImageEnum.KIT_ROBOT_HAND, 60, -1, currentTime, handMove, true);
 		Painter.draw(g, Painter.ImageEnum.ROBOT_ARM_1, 400, -1, currentTime, armMove, true);
+		
+		if (kit != null)
+			kit.draw(g, currentTime);
 	}
 	
 	private void doCalculations(long currentTime)
 	{
-		Point2D.Double target = movement.getStartPos();
+		Point2D.Double target = new Point2D.Double(movement.getStartPos().x - baseStartX, movement.getStartPos().y - baseStartY);
+		
+		double theta = 0;
+		if (target.x != 0 || target.y != 0)
+			theta = Math.asin(target.x/(Math.sqrt(Math.pow(target.x, 2) + Math.pow(target.y, 2))));
+		
+		if (target.y > 0 && target.x < 0)
+			theta -= Math.PI/2;
+		
+		if (armMove.getEndRot() != theta)
+			armMove = Movement.fromSpeed(armMove.calcPos(currentTime), armMove.calcRot(currentTime), currentTime, armMove.calcPos(currentTime), theta, Math.PI/4);
 		
 		armMove.slaveTranslation(baseMove, 0, 0, currentTime);
 		handMove.slaveTranslation(armMove, 180*Math.sin(armMove.calcRot(currentTime)), -180*Math.cos(armMove.calcRot(currentTime)), currentTime);
 		handMove.slaveRotation(armMove, 0, currentTime);
+		
+		if (kit != null)
+		{
+			kit.movement.slaveTranslation(handMove, 0, 0, currentTime);
+			kit.movement.slaveRotation(handMove, 0, currentTime);
+		}
+		
+	}
+	
+	public boolean arrived(long currentTime)
+	{
+		return (armMove.arrived(currentTime) && baseMove.arrived(currentTime) && handMove.arrived(currentTime));
+	}
+	
+	public void setKit(GUIKit kit)
+	{
+		if (this.kit != null)
+		{
+			System.err.println("KitRobot already has a kit!");
+			return;
+		}
+		
+		this.kit = kit;
+	}
+	
+	public GUIKit removeKit()
+	{
+		GUIKit temp = kit;
+		kit = null;
+		return temp;
+	}
+	
+	public void park()
+	{
+		this.movement = new Movement(new Point2D.Double(baseStartX, baseStartY), 0);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
