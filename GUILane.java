d55b38910b3dcd0a651099f531fca5080073a6ed
Clayton <cketner@usc.edu>
2012-11-04 00:47:35 -0700

    Added draw and pause for parts or pallets in the lane

--- GUILane.java
+++ GUILane.java
@@ -1,5 +1,6 @@
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.awt.image.BufferedImage;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -14,14 +15,26 @@ import java.util.ArrayList;
 public class GUILane implements Serializable
 {
 	public ComboLane lane;
-	public Movement movement;
+	public Movement movement; // For storing lane position info
+	
+	boolean isForParts;
+	private ArrayList<GUIPallet> pallets;
+	private ArrayList<GUIPart> topParts, bottomParts;
 	
 	private int laneLength;
 	private ArrayList<GUILaneSegment> guiLaneSegments;
 		
-	public GUILane(ComboLane lane, int laneLength, double x, double y)
+	public GUILane(ComboLane lane, boolean isForParts, int laneLength, double x, double y)
 	{
 		this.lane = lane;
+		
+		this.isForParts = isForParts;
+		if (isForParts)
+		{
+			topParts = new ArrayList<GUIPart>();
+			bottomParts = new ArrayList<GUIPart>();
+		} else pallets = new ArrayList<GUIPallet>();
+		
 		this.laneLength = laneLength;
 		guiLaneSegments = new ArrayList<GUILaneSegment>();
 		movement = new Movement(new Point2D.Double(x,y), 0);
@@ -37,27 +50,7 @@ public class GUILane implements Serializable
 		BufferedImage i_temp = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g_temp = i_temp.createGraphics();
 		
-		if (lane.isLaneOn())
-		{
-			// Start the lane
-			for (GUILaneSegment guiLaneSegment : guiLaneSegments)
-				guiLaneSegment.movement.unPause(currentTime);
-			
-			if (guiLaneSegments.get(0).movement.arrived(currentTime))
-			{
-				// Set each lane segment back to their start position to keep lane animating
-				for (GUILaneSegment guiLaneSegment : guiLaneSegments)
-				{
-					guiLaneSegment.movement = Movement.fromSpeed(guiLaneSegment.movement.getStartPos(), 0, currentTime, guiLaneSegment.movement.getEndPos(), 0, lane.getSpeed());
-				}
-			}
-		} else {
-			// Stop the lane
-			for (GUILaneSegment guiLaneSegment : guiLaneSegments)
-			{
-				guiLaneSegment.movement.pause(currentTime);
-			}
-		}
+		checkMotion(currentTime);
 		
 		// Draw to the bufferedImage
 		for (GUILaneSegment guiLaneSegment : guiLaneSegments)
@@ -75,5 +68,105 @@ public class GUILane implements Serializable
 		Painter.draw(g, Painter.ImageEnum.SHADOW2, 10, 90, currentTime, 
 					 new Movement(new Point2D.Double(movement.getStartPos().x + 60*laneLength-10, movement.getEndPos().y), Math.PI), false);
 		
+		
+		// Draw the pallets or parts
+		if (isForParts)
+		{
+			for (GUIPart p : topParts)
+				p.draw(g, currentTime);
+			for (GUIPart p : bottomParts)
+				p.draw(g, currentTime);
+		} else {
+			for (GUIPallet p : pallets)
+				p.draw(g, currentTime);
+		}
+		
+	}
+	
+	private void checkMotion(long currentTime)
+	{
+		if (lane.isLaneOn())
+		{
+			// Start the lane
+			for (GUILaneSegment guiLaneSegment : guiLaneSegments)
+				guiLaneSegment.movement.unPause(currentTime);
+			
+			if (isForParts)
+			{
+				for (GUIPart p : topParts)
+					p.movement.unPause(currentTime);
+				for (GUIPart p : bottomParts)
+					p.movement.unPause(currentTime);
+			} else {
+				for (GUIPallet p : pallets)
+					p.movement.unPause(currentTime);
+			}
+			
+			if (guiLaneSegments.get(0).movement.arrived(currentTime))
+			{
+				// Set each lane segment back to their start position to keep lane animating
+				for (GUILaneSegment guiLaneSegment : guiLaneSegments)
+				{
+					guiLaneSegment.movement = Movement.fromSpeed(guiLaneSegment.movement.getStartPos(), 0, currentTime, guiLaneSegment.movement.getEndPos(), 0, lane.getSpeed());
+				}
+			}
+		} else {
+			// Stop the lane
+			for (GUILaneSegment guiLaneSegment : guiLaneSegments)
+			{
+				guiLaneSegment.movement.pause(currentTime);
+			}
+			
+			if (isForParts)
+			{
+				for (GUIPart p : topParts)
+					p.movement.pause(currentTime);
+				for (GUIPart p : bottomParts)
+					p.movement.pause(currentTime);
+			} else {
+				for (GUIPallet p : pallets)
+					p.movement.pause(currentTime);
+			}
+		}
+	}
+	
+	public void addPallet()
+	{
+		GUIPallet pallet = new GUIPallet(new Pallet(), new GUIKit(new Kit(), 0,0), movement.getStartPos().x+60*laneLength, movement.getStartPos().y+46);
+		pallets.add(pallet);
+		pallet.movement = Movement.fromSpeed(pallet.movement.getStartPos(), Math.PI/2, System.currentTimeMillis(), new Point2D.Double((movement.getStartPos().x+60), pallet.movement.getStartPos().y), Math.PI/2, lane.getSpeed());
+	}
+	
+	public GUIPallet removePallet(GUIPallet pallet)
+	{
+		return pallets.remove(pallets.indexOf(pallet));
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
