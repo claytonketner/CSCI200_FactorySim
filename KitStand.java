0501408d0ac752ebe80558d692e1c7009c8d2c97
Justin Sanders <justinsa@usc.edu>
2012-11-04 13:13:42 -0800

    added GUIKitCamera object and TreeMap of Kit objects

--- KitStand.java
+++ KitStand.java
@@ -1,12 +1,16 @@
 import java.io.Serializable;
+import java.util.TreeMap;
 
 
 @SuppressWarnings("serial")
 public class KitStand implements Serializable
 {
-
+	TreeMap<Integer,Kit> kits; // 0-1 are positions for incomplete kits, 2 is the inspection position
+	GUIKitCamera guiKitCamera;
+	
 	public KitStand()
 	{
-		
+		kits = new TreeMap<Integer,Kit>();
+		guiKitCamera = new GUIKitCamera( new KitCamera() );
 	}
 }
