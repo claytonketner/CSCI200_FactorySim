aa67887a161f7ae69c2065706bf23fabecc909ae
Clayton <cketner@usc.edu>
2012-11-04 13:17:56 -0800

    Fully functional and ready to turn in!

--- V01.java
+++ V01.java
@@ -2,6 +2,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 
 import javax.swing.JPanel;
 
@@ -15,6 +16,12 @@ public class V01 extends JPanel
 	
 	int paintCount = 0;
 	
+	private enum Status {
+		GETKITFROMINCONVEYOR, PUTKITONSTAND, GETKITFROMSTAND, TAKINGPICTURE, PUTKITONOUTCONVEYOR, IDLE
+	}
+	
+	private Status status = Status.IDLE;
+	
 	public V01()
 	{
 		this.setPreferredSize(new Dimension(800,600));
@@ -24,29 +31,94 @@ public class V01 extends JPanel
 		guiKitDeliveryStation = new GUIKitDeliveryStation(new KitDeliveryStation(), new GUILane(new ComboLane(), false, 8, 300,10), new GUILane(new ComboLane(), false, 3, 300-180, 10), 10, 10);
 		guiKitRobot = new GUIKitRobot(new KitRobot());
 		guiKitStand = new GUIKitStand(new KitStand());
-		
-		GUIKit kit = new GUIKit(new Kit(), 300+180, 0);
-		
+				
 		guiKitDeliveryStation.inConveyor.addPallet();
 	}
 	
 	public void paint(Graphics gfx)
 	{
-		Graphics2D g = (Graphics2D)gfx;
 		long currentTime = System.currentTimeMillis();
+
+		checkStatus(currentTime);
+		
+		Graphics2D g = (Graphics2D)gfx;
 		
 		guiKitDeliveryStation.draw(g, currentTime);
 		guiKitStand.draw(g, currentTime);
 		guiKitRobot.draw(g, currentTime);
-
-		
-		if (paintCount % 100 == 0)
-			if (guiKitDeliveryStation.inConveyor.lane.isLaneOn())
-				guiKitDeliveryStation.inConveyor.lane.turnOff();
-			 else
-				guiKitDeliveryStation.inConveyor.lane.turnOn();
 		
 		paintCount++;
 	}
 	
+	private void checkStatus(long currentTime)
+	{
+		if (status == Status.IDLE && guiKitDeliveryStation.inConveyor.hasFullPalletAtEnd(currentTime))
+		{
+			guiKitRobot.movement = new Movement(guiKitDeliveryStation.inConveyor.getLocationOfEndPallet(currentTime), 0);
+			status = Status.GETKITFROMINCONVEYOR;
+			return;
+		}
+		
+		if (status == Status.GETKITFROMINCONVEYOR && guiKitRobot.arrived(currentTime))
+		{
+			guiKitRobot.setKit(guiKitDeliveryStation.inConveyor.removeEndPalletKit());
+			guiKitRobot.movement = new Movement(guiKitStand.getCameraStationLocation(), 0);
+			status = Status.PUTKITONSTAND;
+			return;
+		}
+		
+		if (status == Status.PUTKITONSTAND && guiKitRobot.arrived(currentTime))
+		{
+			guiKitStand.addKit(guiKitRobot.removeKit(), GUIKitStand.StationNumber.THREE);
+			guiKitRobot.park();
+			status = Status.TAKINGPICTURE;
+			return;
+		}
+		
+		if (status == Status.TAKINGPICTURE && guiKitRobot.arrived(currentTime))
+		{
+			guiKitRobot.movement = new Movement(guiKitStand.getCameraStationLocation(), 0);
+			status = Status.GETKITFROMSTAND;
+			return;
+		}
+		
+		if (status == Status.GETKITFROMSTAND && guiKitRobot.arrived(currentTime))
+		{
+			guiKitRobot.setKit(guiKitStand.removeKit(GUIKitStand.StationNumber.THREE));
+			guiKitRobot.movement = new Movement(guiKitDeliveryStation.getOutConveyorLocation(), 0);
+			status = Status.PUTKITONOUTCONVEYOR;
+			return;
+		}
+		
+		if (status == Status.PUTKITONOUTCONVEYOR && guiKitRobot.arrived(currentTime))
+		{
+			guiKitDeliveryStation.outConveyor.addPallet(new GUIPallet(new Pallet(), guiKitRobot.removeKit(), 
+					guiKitDeliveryStation.outConveyor.movement.getStartPos().x-50+60*guiKitDeliveryStation.outConveyor.getLaneLength(), guiKitDeliveryStation.outConveyor.movement.getStartPos().y-12));
+			guiKitRobot.park();
+			status = Status.IDLE;
+			return;
+		}
+		
+		
+		if (guiKitDeliveryStation.outConveyor.hasFullPalletAtEnd(currentTime))
+		{
+			guiKitDeliveryStation.outConveyor.removeEndPallet();
+			guiKitDeliveryStation.inConveyor.addPallet();
+		}
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
