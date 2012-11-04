a280bee18a450130401f8cc5b6aacd17e5df6bab
antq888 <antq888@yahoo.com>
2012-11-04 11:27:15 -0800

    removed get part method

--- GUIPartsBox.java
+++ GUIPartsBox.java
@@ -13,10 +13,6 @@ public class GUIPartsBox implements Serializable {
 		movement = new Movement(new Point2D.Double(x,y), 0);
 	}
 	
-	public GUIPart getGUIPart(){
-		return part;
-	}
-	
 	public void draw( Graphics2D g, long currentTime ){
 		Painter.draw(g, Painter.ImageEnum.PARTS_BOX, currentTime, movement, false);
 	}
