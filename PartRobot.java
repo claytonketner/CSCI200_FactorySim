795ae7c530675c9db9e071342b45a51e55e07ab9
Justin Sanders <justinsa@usc.edu>
2012-11-04 22:55:45 -0800

    removed add/removePartFromGripper methods

--- PartRobot.java
+++ PartRobot.java
@@ -9,11 +9,5 @@ public class PartRobot implements Serializable {
 		partsInGripper = new TreeMap<Integer, GUIPart>();
 	}
 	
-	public void addPartToGripper ( Integer gripperNumber, GUIPart part ) {
-		partsInGripper.put( gripperNumber, part);
-	}
 	
-	public GUIPart removePartFromGripper ( Integer gripperNumber ) {
-		return partsInGripper.remove( gripperNumber );
-	}
 }
