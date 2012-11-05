8e1a5db04c29ef8b2031051f9e74d6f597c88841
Justin Sanders <justinsa@usc.edu>
2012-11-04 17:43:44 -0800

    added methods from KitRobot

--- PartRobot.java
+++ PartRobot.java
@@ -1,16 +1,11 @@
-import java.awt.geom.Point2D;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class PartRobot implements Serializable {
 	ArrayList<Part> partsInGripper;
-	Point2D.Double position;
-	Movement movement;
 	
 	public PartRobot() {
 		partsInGripper = new ArrayList<Part>();
-		position = new Point2D.Double( 300, 300 ); //these values are random
-		movement = new Movement( position, 0 );
 	}
 }
