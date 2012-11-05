28d3a70977a207236c8a4044f4497a00aed9386c
Clayton <cketner@usc.edu>
2012-11-04 22:03:58 -0800

    Added methods for drawing and seeing if it has expired

--- GUIKitCamera.java
+++ GUIKitCamera.java
@@ -1,11 +1,31 @@
+import java.awt.Graphics2D;
 import java.io.Serializable;
 
 
 @SuppressWarnings("serial")
-public class GUIKitCamera implements Serializable {
+public class GUIKitCamera implements Serializable 
+{
 	KitCamera kitCamera;
+	Movement movement;
+	long birthTime, lifeLength;
 	
-	public GUIKitCamera ( KitCamera kitCamera ) {
+	public GUIKitCamera ( KitCamera kitCamera, Movement movement, long currentTime, long lifeLength ) {
 		this.kitCamera = kitCamera;
+		this.movement = movement;
+		this.birthTime = currentTime;
+		this.lifeLength = lifeLength;
+	}
+	
+	public boolean isExpired(long currentTime)
+	{
+		if (birthTime - currentTime > lifeLength)
+			return true;
+		return false;
+	}
+	
+	public void draw(Graphics2D g, long currentTime)
+	{
+		if (!isExpired(currentTime))
+			Painter.draw(g, Painter.ImageEnum.CAMERA_FLASH, currentTime, movement, true);
 	}
 }
