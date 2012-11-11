65fc31052e8e9857934ad2e1ca8038204506d74e
Andrew Downing <ad510@users.sourceforge.net>
2012-11-10 16:02:34 -0800

    add constructor for empty PartListMsg

--- PartListMsg.java
+++ PartListMsg.java
@@ -6,6 +6,12 @@ public class PartListMsg implements Serializable {
 	/** ArrayList of available parts */
 	public ArrayList<Part> parts;
 
+	/** constructor for empty PartListMsg */
+	public PartListMsg() {
+		parts = new ArrayList<Part>();
+	}
+
+	/** constructor for non-empty PartListMsg */
 	public PartListMsg(ArrayList<Part> newParts) {
 		parts = newParts;
 	}
