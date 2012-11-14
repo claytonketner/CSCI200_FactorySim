fe29d102c5d55b15a1a6d8d5c5f6e3b7dfe84f8b
Cullon Hecox <checox@usc.edu>
2012-11-13 18:28:02 -0800

    edits to GUIBin and Bin

--- Bin.java
+++ Bin.java
@@ -1,12 +1,25 @@
+import java.util.ArrayList;
+
 
 public class Bin 
 {
 	public Part part;
-	private boolean binEmpty;
+	private boolean binEmpty = false;
 	
-	public Bin()
+	public Bin(Part p)
 	{
-		part = new Part();
+		part = new Part(p.getName(), p.getDescription(), p.getNumber());
+	}
+	
+	public void setPart( Part p )
+	{
+		part.setDescription(p.getDescription());
+		part.setName(p.getName());
+		part.setNumber(p.getNumber());
 	}
 
+	public void dumpBin()
+	{
+		binEmpty = true;
+	}
 }
--- GUIBin.java
+++ GUIBin.java
@@ -1,21 +1,37 @@
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 import java.io.Serializable;
+import java.util.Map;
 
 @SuppressWarnings("serial")
-public class GUIBin implements Serializable {
-	public Movement movement;
+public class GUIBin implements Serializable 
+{
 	public GUIPart part;
 	public Bin bin;
+	public Point2D.Double currPos;
+	private Movement binMove;
 	
 	public GUIBin( GUIPart gp, Bin bin, double x, double y )
 	{
 		part = gp;
-		movement = new Movement(new Point2D.Double(x,y), 0);
+		currPos = new Point2D.Double(x,y);
+		binMove = new Movement(currPos, 0);
 		this.bin = bin;
 	}
 	
-	public void draw( Graphics2D g, long currentTime ){
-		Painter.draw(g, Painter.ImageEnum.PARTS_BOX, currentTime, movement, false);
+	public void draw( Graphics2D g, long currentTime )
+	{
+		calculate(currentTime);
+		Painter.draw(g, Painter.ImageEnum.PARTS_BOX, currentTime, binMove, false);
+	}
+	
+	private void calculate(long currentTime)
+	{
+		
+	}
+	
+	public boolean inPosition(long currentTime)
+	{
+		return binMove.arrived(currentTime);
 	}
 }
