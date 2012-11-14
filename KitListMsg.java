ec00efe8d3715ca5cd7045d1f864cc5944acb44a
Cullon Hecox <checox@usc.edu>
2012-11-13 20:32:42 -0800

    Comments and minor edits added to KitListMsg.java and KitsClient.java

--- KitListMsg.java
+++ KitListMsg.java
@@ -6,10 +6,12 @@ public class KitListMsg implements Serializable {
 	/** ArrayList of available kits */
 	public ArrayList<Kit> kits;
 	
+	/** Constructor for empty list of kits **/ 
 	public KitListMsg() {
 		kits = new ArrayList<Kit>();
 	}
 
+	/** constructor for overwriting existing list of kits **/
 	public KitListMsg(ArrayList<Kit> newKits) {
 		kits = newKits;
 	}
