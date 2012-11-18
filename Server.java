c11fd80aa3c946f6de9cc1b2c179737969b02ecf
Andrew Downing <ad510@users.sourceforge.net>
2012-11-17 18:56:06 -0800

    create a FactoryControlManager window when Server starts up

--- Server.java
+++ Server.java
@@ -47,6 +47,8 @@ public class Server implements ActionListener, Networked {
 	private ProduceStatusMsg status;
 	/** current factory state */
 	private FactoryStateMsg state;
+	/** GUI window for user to control the factory */
+	private FactoryControlManager controller;
 
 	/** constructor for server class */
 	public Server() throws IOException {
@@ -59,22 +61,12 @@ public class Server implements ActionListener, Networked {
 		}
 		// load factory settings from file (or set up new factory if can't load from file)
 		loadSettings();
+		// open controller window
+		controller = new FactoryControlManager(this);
 		// start update timer
 		new javax.swing.Timer(UPDATE_RATE, this).start();
-		System.out.println("Server is ready; press ctrl+C to exit");
 		// wait for clients to connect
-		while (true) { // loop exits when user presses ctrl+C
-			try {
-				Socket socket = serverSocket.accept();
-				netComms.add(new NetComm(socket, this));
-				wants.add(new ClientWants());
-				System.out.println("Client " + (netComms.size() - 1) + " has joined");
-			}
-			catch (Exception ex) {
-				System.out.println("Error accepting new client connection");
-				ex.printStackTrace();
-			}
-		}
+		new Thread(new AcceptClientsThread()).start();
 	}
 
 	public static void main(String[] args) {
@@ -421,6 +413,7 @@ public class Server implements ActionListener, Networked {
 		return true;
 	}
 
+	/** apply update to factory state on server and all clients that requested it */
 	private void applyUpdate(FactoryUpdateMsg update) {
 		for (int i = 0; i < wants.size(); i++) {
 			if (wants.get(i).state) {
@@ -576,6 +569,25 @@ public class Server implements ActionListener, Networked {
 		catch (Exception ex) {
 			System.out.println("Error saving factory settings to file.");
 			System.out.println("Make sure the \"save\" folder exists.");
+		}
+	}
+
+	/** thread to accept new clients */
+	private class AcceptClientsThread implements Runnable {
+		/** wait for clients to connect */
+		public void run() {
+			while (true) { // loop exits when user presses ctrl+C
+				try {
+					Socket socket = serverSocket.accept();
+					netComms.add(new NetComm(socket, Server.this));
+					wants.add(new ClientWants());
+					System.out.println("Client " + (netComms.size() - 1) + " has joined");
+				}
+				catch (Exception ex) {
+					System.out.println("Error accepting new client connection");
+					ex.printStackTrace();
+				}
+			}
 		}
 	}
 }
