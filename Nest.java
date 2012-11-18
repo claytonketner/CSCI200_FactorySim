786d21d0c5410890502a1623a7e57106f0c14ada
Tianbo Li <tianboli@usc.edu>
2012-11-18 10:06:38 -0800

    added comments in nest

--- Nest.java
+++ Nest.java
@@ -1,23 +1,26 @@
 import java.io.*;
 import java.util.ArrayList;
-
+/** class constructs basic functionality of nest */
 public class Nest implements Serializable {
-	private final int limit = 10; //instructions say 1-10 parts per nest
-	
+	/** instructions say 1-10 parts per nest */
+	private final int limit = 10; 
+	/** parts are in the nest */
 	public ArrayList<Part> nestedItems;
+	/** true if nest is full */
 	private boolean nestFull;
+	/** To Do: write description */
 	private boolean mySwitch;
-
+	/** Initialization */
 	public Nest(){
 		nestedItems = new ArrayList<Part>();
 		nestFull = false;
 		mySwitch = false;
 	}
-	
+	/** true if nest is full */
 	public boolean isNestFull(){
 		return nestFull;
 	}
-	
+	/** load parts into nest */
 	public boolean addPart( Part p ){ //returns true if add was successful
 		if(nestedItems.size() < limit) {
 			nestedItems.add(p);
@@ -31,7 +34,7 @@ public class Nest implements Serializable {
 			return false;
 		}
 	}
-	
+	/** remove part from nest */
 	public Part removePart(){
 		if( nestedItems.size() > 0 ){
 			nestFull = false;
@@ -40,12 +43,12 @@ public class Nest implements Serializable {
 			return null;
 		}
 	}
-	
+	/** dump nest out */
 	public void dumpNest(){
 		nestedItems = new ArrayList<Part>();
 		nestFull = false;
 	}
-	
+	/** To Do: write description */
 	public void flipSwitch(){
 		mySwitch = !mySwitch;
 	}
