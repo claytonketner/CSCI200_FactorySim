2016ac2091ce4f84688034e70a5dcdd9d96dc2e6
Tianbo Li <tianboli@usc.edu>
2012-11-18 10:29:11 -0800

    added comments in Lane and ComboLane

--- Lane.java
+++ Lane.java
@@ -4,48 +4,51 @@ import java.util.ArrayList;
 
 public class Lane implements Serializable
 {
+	/** speed equals to 80 */
 	private double speed = 80;
-	
+	/** true if lane is on */
 	private boolean laneOn;
+	/** parts are in the lane */
 	private ArrayList<Part> parts;
+	/** amplitude */
 	private double amplitude;
-	
+	/** Initialization */
 	public Lane()
 	{
 		laneOn = true;
 		amplitude = 1;
 	}
-	
+	/** true if lane is on */
 	public boolean isLaneOn()
 	{
 		return laneOn;
 	}
-	
+	/** turn off lane (set laneOn to false) */
 	public void turnOff()
 	{
 		laneOn = false;
 	}
-	
+	/** turn on lane (set laneOn to true) */
 	public void turnOn()
 	{
 		laneOn = true;
 	}
-	
+	/** set amplitude */
 	public void setAmplitude(double amplitude)
 	{
 		this.amplitude = amplitude;
 	}
-	
+	/** return amplitude */
 	public double getAmplitude()
 	{
 		return amplitude;
 	}
-	
+	/** add a part to lane */
 	public void addPart(Part p)
 	{
 		parts.add(p);
 	}
-	
+	/** remove the last part from lane */
 	public Part removePart(){
 		if( parts.size() > 0 ){ //assumes Clayton's graphics depends only on the number of parts on the lane
 			return parts.remove( parts.size() - 1 );
@@ -65,7 +68,7 @@ public class Lane implements Serializable
 //		parts.remove(index);
 //		return p;
 //	}
-	
+	/** return speed */
 	public double getSpeed()
 	{
 		return speed;
