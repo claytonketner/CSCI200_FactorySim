35fda9bdcf653ad23df384ad2459b82f0bca599b
Tianbo Li <tianboli@usc.edu>
2012-11-18 14:14:40 -0800

    added comment in DeleteKitMsg

--- DeleteKitMsg.java
+++ DeleteKitMsg.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class DeleteKitMsg implements Serializable {
 	/** kit number of kit to delete */
 	public int number;
-
+	/** constructor to set up DeleteKitMsg with the number of the kit that should be deleted */
 	public DeleteKitMsg(int delNumber) {
 		number = delNumber;
 	}
