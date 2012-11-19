d466f98b57dd33ca0c3826e2e86bace9e0b13b6e
Tianbo Li <tianboli@usc.edu>
2012-11-18 20:53:14 -0800

    added comments in GUIKitdeliverystation

--- GUIKitDeliveryStation.java
+++ GUIKitDeliveryStation.java
@@ -7,70 +7,75 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
-
 @SuppressWarnings("serial")
-public class GUIKitDeliveryStation implements GUIItem, Serializable
-{
+public class GUIKitDeliveryStation implements GUIItem, Serializable {
+	/** used to access delivery station data */
 	public KitDeliveryStation kitDeliveryStation;
+	/** used to access movement data */
 	public Movement movement;
+	/** conveyor lanes */
 	public GUILane inConveyor, outConveyor;
-	
+	/** GUI pallets */
 	private ArrayList<GUIPallet> guiPallets;
-	
-	
-	public GUIKitDeliveryStation(KitDeliveryStation kitDeliveryStation, GUILane inConveyor, GUILane outConveyor, double x, double y)
-	{
+
+	/** Constructor */
+	public GUIKitDeliveryStation(KitDeliveryStation kitDeliveryStation,
+			GUILane inConveyor, GUILane outConveyor, double x, double y) {
 		this.kitDeliveryStation = kitDeliveryStation;
 		this.inConveyor = inConveyor;
 		this.outConveyor = outConveyor;
-		this.movement = new Movement(new Point2D.Double(x,y), 0);
+		this.movement = new Movement(new Point2D.Double(x, y), 0);
 	}
-	
-	public GUIKitDeliveryStation(KitDeliveryStation kitDeliveryStation, GUILane inConveyor, GUILane outConveyor, Movement movement)
-	{
+
+	/** Constructor */
+	public GUIKitDeliveryStation(KitDeliveryStation kitDeliveryStation,
+			GUILane inConveyor, GUILane outConveyor, Movement movement) {
 		this.kitDeliveryStation = kitDeliveryStation;
 		this.inConveyor = inConveyor;
 		this.outConveyor = outConveyor;
 		this.movement = movement;
 	}
 
-	public void draw(Graphics2D g, long currentTime)
-	{	
+	/** draws the kit delivery station */
+	public void draw(Graphics2D g, long currentTime) {
 		checkStatus(currentTime);
-		
+
 		inConveyor.draw(g, currentTime);
 		outConveyor.draw(g, currentTime);
 	}
 
-	public void turnOn(long currentTime)
-	{
+	/** always turn on conveyor */
+	public void turnOn(long currentTime) {
 		inConveyor.turnOn(currentTime);
 		outConveyor.turnOn(currentTime);
 	}
-	
-	private void checkStatus(long currentTime)
-	{
-		if (inConveyor.shouldReset(currentTime)) inConveyor.reset(currentTime);
-		if (outConveyor.shouldReset(currentTime)) outConveyor.reset(currentTime);
-		if (inConveyor.hasEmptyPalletAtEnd(currentTime))
-		{
+
+	/**
+	 * if this conveyor is full, turn off the lane, if this conveyor is empty,
+	 * turn this lane on and move all of the pallets down one space
+	 */
+	private void checkStatus(long currentTime) {
+		if (inConveyor.shouldReset(currentTime))
+			inConveyor.reset(currentTime);
+		if (outConveyor.shouldReset(currentTime))
+			outConveyor.reset(currentTime);
+		if (inConveyor.hasEmptyPalletAtEnd(currentTime)) {
 			inConveyor.removeEndPallet(currentTime);
 			inConveyor.turnOn(currentTime);
 		}
-		if (inConveyor.hasFullPalletAtEnd(currentTime))
-		{
+		if (inConveyor.hasFullPalletAtEnd(currentTime)) {
 			inConveyor.turnOff(currentTime);
 		}
 	}
-	
-	public Point2D.Double getOutConveyorLocation()
-	{
-		return new Point2D.Double(outConveyor.getPos().x + outConveyor.getLength() - 50, outConveyor.getPos().y + 60);
+
+	/** return outConveyor's location */
+	public Point2D.Double getOutConveyorLocation() {
+		return new Point2D.Double(outConveyor.getPos().x
+				+ outConveyor.getLength() - 50, outConveyor.getPos().y + 60);
 	}
 
 	/** setter for movement */
-	public void setMove(Movement movement)
-	{
+	public void setMove(Movement movement) {
 		this.movement = movement;
 	}
 
